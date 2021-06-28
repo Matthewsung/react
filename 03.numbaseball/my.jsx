7a66b79703fb1692d78a331ea54d20b64138c15b
juyeongsung <jooyongs1@gmail.com>
2021-06-28 19:08:17 +0900

    6.28

--- 03.numbaseball/my.jsx
+++ 03.numbaseball/my.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { PureComponent, useState ,memo} from 'react';
 import Try2 from './my2';
 
 function getNum (){
@@ -11,126 +11,135 @@ function getNum (){
   }
   return num
 }
-const My =()=>{
+// const My = memo(()=>{
     
-  const [result,setresult] = useState('');
-  const [value,setValue]=useState('');
-  const [answer,setAnswer]=useState(getNum());
-  const [tries,setTries]=useState([]);
+//   const [result,setresult] = useState('');
+//   const [value,setValue]=useState('');
+//   const [answer,setAnswer]=useState(getNum());
+//   const [tries,setTries]=useState([]);
 
-  const onSubmitForm = (e)=>{
-    e.preventDefault();
-      if(answer.join('') === value){
-        setresult('홈런입니다!')
-        alert("정답입니다")
-        setValue('')
-        setAnswer(getNum())
-        setTries([])
-      }
-      else{
-        let strike = 0;
-        let ball = 0;
-        for(let i =0; i < value.length; i++){
-          if(answer.join('').indexOf(value[i]) > -1){
-            answer.join('').indexOf(value[i]) === i?strike += 1:ball += 1;
-          }
-        }
-        setTries((prevState)=>{
-          return(
-            [...prevState, {try:value, result:`${strike}스트라이크 ${ball}볼입니다`}]
-            )
-          })
-        setValue('')
-      }
-      console.log(answer.join('') , value)
-  }
+//   const onSubmitForm = (e)=>{
+//     e.preventDefault();
+//       if(answer.join('') === value){
+//         setresult('홈런입니다!')
+//         alert("정답입니다")
+//         setValue('')
+//         setAnswer(getNum())
+//         setTries([])
+//       }
+//       else{
+//         let strike = 0;
+//         let ball = 0;
+//         for(let i =0; i < value.length; i++){
+//           if(answer.join('').indexOf(value[i]) > -1){
+//             answer.join('').indexOf(value[i]) === i?strike += 1:ball += 1;
+//           }
+//         }
+//         setTries((prevState)=>{
+//           return(
+//             [...prevState, {try:value, result:`${strike}스트라이크 ${ball}볼입니다`}]
+//             )
+//           })
+//         setValue('')
+//       }
+//       console.log(answer.join('') , value)
+//   }
 
-  const onChangeInput = (e)=>{
-    setValue(e.target.value)
+//   const onChangeInput = (e)=>{
+//     setValue(e.target.value)
 
-  };
-  return(
-    <>
-        <h1>{result}</h1>
-        <form onSubmit={onSubmitForm}>
-          <input onChange={onChangeInput} value = {value}/>
-        </form>
-        <ul>
-          {tries.map((v,index)=>{
-            return(
-              <Try2 key={index} Try={v} />
-            )
-          })}
-        </ul>
-      </>
-  )
-}
-// class My extends Component{
-//   // state = {
-//   //   result:'',
-//   //   value:'',
-//   //   answer:getNum(),
-//   //   tries:[]
-//   // };
-
-
-//   // onSubmitForm = (e) => { 
-//   //   e.preventDefault();
-//   //   if(this.state.answer.join('') === this.state.value){
-//   //     this.setState({
-//   //       result:'홈런입니다!'
-//   //     })
-//   //     alert("정답입니다")
-//   //     this.setState({
-//   //       value:'',
-//   //       answer:getNum(),
-//   //       tries:[]
-//   //     })
-//   //   }
-//   //   else{
-//   //     let strike = 0;
-//   //     let ball = 0;
-//   //     for(let i =0; i < this.state.value.length; i++){
-//   //       if(this.state.answer.join('').indexOf(this.state.value[i]) > -1){
-//   //         this.state.answer.join('').indexOf(this.state.value[i]) === i?strike += 1:ball += 1;
-//   //       }
-//   //     }
-//   //     this.setState((prevState)=>{
-//   //       return {
-//   //         tries: [...prevState.tries, {try:this.state.value, result:`${strike}스트라이크 ${ball}볼입니다`}],
-//   //         value:''
-//   //       }
-//   //     })
-//   //   }
-//   //   console.log(this.state.answer.join('') , this.state.value)
-//   // };
-
-
-//   // onChangeInput =(e)=>{
-//   //   console.log();
-//   //   this.setState({
-//   //     value:e.target.value
-//   //   })
-//   // };
-
-
-//   render() {
-//     return (
-//       <>
-//         <h1>{this.state.result}</h1>
-//         <form onSubmit={this.onSubmitForm}>
-//           <input onChange={this.onChangeInput} value = {this.state.value}/>
+//   };
+//   return(
+//     <>
+//         <h1>{result}</h1>
+//         <form onSubmit={onSubmitForm}>
+//           <input onChange={onChangeInput} value = {value}/>
 //         </form>
 //         <ul>
-//           {this.state.tries.map((v,index)=>{
+//           {tries.map((v,index)=>{
 //             return(
 //               <Try2 key={index} Try={v} />
 //             )
 //           })}
 //         </ul>
 //       </>
-//     )
-//   }
-// }
+//   )
+// })
+
+
+
+
+
+
+
+
+
+
+class My extends PureComponent{
+  state = {
+    result:'',
+    value:'',
+    answer:getNum(),
+    tries:[]
+  };
+
+  onSubmitForm = (e) => { 
+    e.preventDefault();
+    if(this.state.answer.join('') === this.state.value){
+      this.setState({
+        result:'홈런입니다!'
+      })
+      alert("정답입니다")
+      this.setState({
+        value:'',
+        answer:getNum(),
+        tries:[]
+      })
+    }
+    else{
+      let strike = 0;
+      let ball = 0;
+      for(let i =0; i < this.state.value.length; i++){
+        if(this.state.answer.join('').indexOf(this.state.value[i]) > -1){
+          this.state.answer.join('').indexOf(this.state.value[i]) === i?strike += 1:ball += 1;
+        }
+      }
+      this.setState((prevState)=>{
+        return {
+          tries: [...prevState.tries, {try:this.state.value, result:`${strike}스트라이크 ${ball}볼입니다`}],
+          value:''
+        }
+      })
+    }
+    console.log(this.state.answer.join('') , this.state.value)
+  };
+
+
+  onChangeInput =(e)=>{
+    console.log();
+    this.setState({
+      value:e.target.value
+    })
+  };
+
+  input
+  render() {
+    return (
+      <>
+        <h1>{this.state.result}</h1>
+        <form onSubmit={this.onSubmitForm}>
+          <input onChange={this.onChangeInput} value = {this.state.value}/>
+        </form>
+        <ul>
+          {this.state.tries.map((v,index)=>{
+            return(
+              <Try2 key={index} Try={v} />
+            )
+          })}
+        </ul>
+      </>
+    )
+  }
+}
 
 export default My
--- 03.numbaseball/my2.jsx
+++ 03.numbaseball/my2.jsx
@@ -1,12 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component , memo } from 'react';
 
-class Try2 extends Component {
-
-  render(){
-    return (
-      <li><b>{this.props.Try.try}</b>는 {this.props.Try.result} </li>
-    )
-  }
-}
+const Try2 = memo(({Try})=>{
+  return (
+    <li>
+        <b>{Try.try}</b>는 {Try.result} 
+      </li>
+  )
+})
+// class Try2 extends Component {
+//   render(){
+//     return (
+//       <li>
+//         <b>{this.props.Try.try}</b>는 {this.props.Try.result} 
+//       </li>
+//     )
+//   }
+// }
 
 export default Try2
