17cba465d75c52b84c89a82b77c282ed0d91ee43
juyeongsung <jooyongs1@gmail.com>
2021-07-24 16:43:52 +0900

    7.24

--- 00.test/src/App.js
+++ 00.test/src/App.js
@@ -1,11 +1,6 @@
 import './App.css';
-import { useEffect, useState } from 'react';
-import Test_1 from './Test_1';
 
 function App() {
-  const [ttt, aaa] = useState([])
-  console.log(Test_1().Countries)
-  aaa(Test_1().Countries)
     
   return (
     <div className="App">
