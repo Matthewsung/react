5b3f808fe1b29f14340add4f304a4e1199f73752
juyeongsung <jooyongs1@gmail.com>
2021-07-27 18:48:32 +0900

    7,27

--- gugudan_project/src/App.js
+++ gugudan_project/src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
 import Gugudan from './component/Gugudan';
-import {Route,BrowserRouter,Switch} from 'react-router-dom'
 function App() {
   return (
-    <BrowserRouter>
       <div className="App">
-        <Route exact path="/">
           <Gugudan />
-        </Route>
       </div>
-    </BrowserRouter>
   );
 }
 
