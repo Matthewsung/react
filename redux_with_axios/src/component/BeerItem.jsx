945b127a793c6bcb95e604d31e1e45e88a89f4a0
juyeongsung <jooyongs1@gmail.com>
2021-08-10 00:11:09 +0900

    8.9

--- redux_with_axios/src/component/BeerItem.jsx
+++ redux_with_axios/src/component/BeerItem.jsx
@@ -17,10 +17,31 @@ export default function BeerItem({data}) {
         <TableCell>{data.abv}</TableCell>
         <TableCell>{`${data.volume.value} ${data.volume.unit} `}</TableCell>
       </TableRow>
-      <Modal style={{display:showModal}} />
+      <Modal style={{display:showModal}} onClick={()=>setShowModal('none')}>
+        <InnerModal>
+          <h1>{data.name}</h1>
+          <div>
+            <img src={data.image_url} alt={data.name} />
+          </div>
+        </InnerModal>
+      </Modal>
         
     </>
   )
 }
 
-
+const Modal = styled.div`
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+  background: rgba(0, 0, 0, 0.3);
+  position: fixed;
+  left: 0;
+  top: 0;
+`
+const InnerModal =styled.div `
+  width:1000px;
+  height: 500px;
+  background: #fff;
+  margin: 5rem auto;
+`
--- redux_with_axios/src/component/Head.jsx
+++ redux_with_axios/src/component/Head.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 export default function Head() {
   return (
     <>
-      <Link to='/home'>
-        <h1>HOME</h1>
-      </Link>
-      <Link to='/beerlist'>
-        <h1>BeerList</h1>
-      </Link>
+      <Header>
+        <Link to='/home'>
+          <h1>HOME</h1>
+        </Link>
+        <Link to='/beerlist'>
+          <h1>BeerList</h1>
+        </Link>
+      </Header>
     </>
     )
 }
+const Header = styled.div`
+  width:100%;
+  display:flex;
+  
+`
